c7a91f3941a55b0a03d268b044882ad5e3d35b01
Favvie <favourabangwu@gmail.com>
2024-06-22 16:13:08 +0100

    fix: refactor code

--- src/utils/cipherUtils.js
+++ src/utils/cipherUtils.js
@@ -1,17 +1,21 @@
-// src/utils/cipherUtils.js
 export const caesarEncrypt = (text, shift) => {
-	return text.replace(/[a-z]/gi, (char) => {
-		let code = char.charCodeAt(0);
+	return text
+		.split("")
+		.map((char) => {
+			if (char.match(/[a-z]/i)) {
+				let code = char.charCodeAt(0);
 
-		if (code >= 65 && code <= 90) {
-			return String.fromCharCode(((code - 65 + shift) % 26) + 65);
-		} else if (code >= 97 && code <= 122) {
-			return String.fromCharCode(((code - 97 + shift) % 26) + 97);
-		}
-		return char;
-	});
+				if (code >= 65 && code <= 90) {
+					return String.fromCharCode(((((code - 65 + shift) % 26) + 26) % 26) + 65);
+				} else if (code >= 97 && code <= 122) {
+					return String.fromCharCode(((((code - 97 + shift) % 26) + 26) % 26) + 97);
+				}
+			}
+			return char;
+		})
+		.join("");
 };
 
 export const caesarDecrypt = (text, shift) => {
-	return caesarEncrypt(text, 26 - shift);
+	return caesarEncrypt(text, -shift);
 };
